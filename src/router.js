a9628d3199639e75ae655a3f9779dd76121950ec
guille <guille@localhost.localdomain>
2019-02-18 22:19:05 -0300

    configuracion inicial del router

--- src/router.js
+++ src/router.js
@@ -1,25 +1,39 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import ListaUsuario from './components/Usuarios/ListaUsuario.vue'
+import ListaRoles from './components/Roles/ListaRoles.vue'
 
-Vue.use(Router)
+Vue.use(Router);
 
-export default new Router({
-  mode: 'history',
+var router = new Router({
   base: process.env.BASE_URL,
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: Home
+      path: "/",
+      name: "home",
+      component: Home,
+      meta: {libre: true}
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
+      path: "/usuarios",
+      name: "usuarios",
+      component: ListaUsuario,
+      meta: {libre: true}
+    },
+    {
+      path: "/roles",
+      name: "roles",
+      component: ListaRoles,
+      meta: {libre: true}
+    },
   ]
-})
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.libre)) {
+    next();
+  }
+});
+
+export default router;
