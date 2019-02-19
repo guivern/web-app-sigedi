9e5f86a1fa871d8dd99b0658dfe16249d98537dd
guille <guille@localhost.localdomain>
2019-02-18 22:18:42 -0300

    componente usuario inicial

---
+++ src/components/Usuarios/ListaUsuario.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-layout align-start>
+        <v-flex>
+            Proximamente Lista Usuarios
+        </v-flex>
+    </v-layout>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
